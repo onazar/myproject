457d679570f7fa030b01e15ce51d126eb77f6629
onazar <onazar@solanolabs.com>
2015-09-21 11:24:22 +0000

    20150921

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionController::TestCase
-  test "should get homepage" do
-    get :homepage
-    assert_response :success
-  end
+class WelcomeControllerTest  ActionController::TestCase
+  #test "should get homepage" do
+  #  get :homepage
+  #  assert_response :success
+  #end
+
+  a = { "x" => 1, "y" => 2, "z" => 3 }.map{ |k,v| k.to_sym => v }
 
 end
